13e2947978da93ea56752cc015ce2646ce27c1b5
Vlad Spinul <shinokakaov@mail.ru>
2025-07-15 16:52:40 +0300

    Добавлены тесты на покрытие новой функциональности класса Category.

--- tests/test_category.py
+++ tests/test_category.py
@@ -30,3 +30,38 @@ def test_category_class_object_creation(a_test_product):
     # Проверка значений атрибутов класса после ввода второй категории
     assert Category.category_count == 2
     assert Category.product_count == 3
+
+
+def test_add_product_method(a_test_product):
+    """ Тестируем работу метода add_product. """
+    category_1 = Category("Category name UNO",
+                          "test description #1",
+                          [a_test_product, a_test_product])
+
+    assert len(category_1.products_list) == 2
+
+    category_1.add_product(a_test_product)
+
+    assert len(category_1.products_list) == 3
+
+
+def test_products_info_str(a_test_product):
+    """ Тестирование работы геттера .products_info_str """
+    category_1 = Category("Category name UNO",
+                          "test description #1",
+                          [a_test_product, a_test_product])
+
+    result = category_1.products_info_str
+    expected_str = "test product, 100.0 руб. Остаток: 5 шт.\ntest product, 100.0 руб. Остаток: 5 шт."
+
+    assert result == expected_str
+
+
+def test_products_info_str_empty():
+    """ Тестируем работу геттера с категорией с пустым списком товаров. """
+    category_1 = Category("Category name UNO",
+                          "test description #1",
+                          [])
+
+    result = category_1.products_info_str
+    assert result == ""
