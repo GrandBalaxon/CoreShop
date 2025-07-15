6af7069e1fda0a0498ec2240e72e80f7eed0df40
Vlad Spinul <shinokakaov@mail.ru>
2025-07-15 13:01:06 +0300

    cls метод new_product готов + гет/сеттер price

--- src/product.py
+++ src/product.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 class Product:
@@ -11,35 +11,56 @@ class Product:
         quantity (int): Количество товара на складе
     """
 
-    __products_list = []
+    __products_list: List["Product"] = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
         Product.__products_list.append(self)
 
     @classmethod
     def new_product(cls, product_data: Dict[str, Any]):
-        """ Создает новый объект Product на основе словаря данных. """
-        new_product = cls(
-            name=product_data["name"],
-            description = product_data["description"],
-            price = product_data["price"],
-            quantity = product_data["quantity"]
-        )
+        """
+        Создает новый объект Product на основе словаря данных.
 
-        for pr in cls.__products_list:
-            if pr.name == new_product.name:
+        :param product_data: Словарь с данными о новом продукте.
+        Должен обязательно содержать ключи: name, description, price, quantity.
+
+        :return: Новый или обновленный объект Product.
+        """
+        name = product_data["name"]
+        description = product_data["description"]
+        price = product_data["price"]
+        quantity = product_data["quantity"]
+
+        for product in cls.__products_list:
+            if product.name == name:
                 # Обновляем цену, если у нового товара она выше
-                if new_product.price > pr.price:
-                    pr.price = new_product.price
-
+                if price > product.price:
+                    product.price = price
                 # Обновляем количество товаров на складе
-                pr.quantity += new_product.quantity
+                product.quantity += quantity
 
-                return pr
+                return product
 
-        return new_product
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        """ Геттер для получения цены товара. """
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        """
+        Сеттер для установки новой цены товара.
+
+        :param new_price: Новая цена. Должна быть больше нуля, иначе выводится сообщение об ошибке.
+        """
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
